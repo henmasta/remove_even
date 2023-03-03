b590bac0784076bb43a9ada0ae39063a4272be52
Henmasta <39940952+henmasta@users.noreply.github.com>
2023-03-03 10:41:06 +0300

    Update MainClass.cpp

--- MainClass.cpp
+++ MainClass.cpp
@@ -2,24 +2,22 @@
 #include <stdio.h>
 #include <iostream>
 #include "MainClass.h"
-#define STACK struct STACK
 
 using namespace std;
-
-STACK {
-        int info;
-        STACK *next;
-};
 
 CListRemoveEveryN::CListRemoveEveryN() {
     head = NULL;
 }
 
+CListRemoveEveryN::~CListRemoveEveryN() {
+    ReleaseList();
+}
 
 void CListRemoveEveryN::ReleaseList() {
-    if (head!=NULL) {
+    
+    if (head) {
         STACK *p;
-        while (head!=NULL) {
+        while (head) {
             p = head;
             head = head->next;
             delete p;
@@ -30,27 +28,91 @@ void CListRemoveEveryN::ReleaseList() {
 
 void CListRemoveEveryN::print() {
     
-	while (head) {
-        cout << head->info << "->";
-        head = head->next;
+    STACK *p = head;
+    
+	while (p) {
+        cout << p->info << "->";
+        p = p->next;
     }
+    
 
-    printf("NULL");
+    cout << "NULL\n";
+    cin.get();
 }
 
 void CListRemoveEveryN::push(int s) {
 
     STACK *newItem;
     newItem = new STACK;
-
-	newItem -> info = s;
-    newItem -> next = head;
-	head = newItem;
-}
-
-STACK CListRemoveEveryN::*remove_even(int n) {
+    newItem->info = s;
     
-    return head;
+    if (!head) {
+        head = newItem;
+        head->next = NULL;
+    } else {
+        newItem -> next = head;
+        head = newItem;
+    }
+    
 }
 
-CListRemoveEveryN::~CListRemoveEveryN(){}
+void CListRemoveEveryN::remove_even(int n) {
+    
+    ReverseStack();
+    STACK* t = head;
+	STACK* t1 = head;
+	int i = 1;
+	while (t) {
+	    
+    	if (i % n != 0) {
+    		t1 = t;
+    		t = t->next;
+    	} else {
+    		if (t == head) {
+    			head = head->next;
+    			delete t;
+    		} else {
+    			t1->next = t->next;
+    			delete t;
+    			t = t1->next;
+    		}   
+    	}
+        ++i;
+	}
+
+    cout << "Stack after function: " << endl;
+    print();
+}
+
+
+void CListRemoveEveryN::ReverseStack() {  
+
+    int tmp[100], i = 0;
+    int g = counter();
+    STACK *p = head;
+    STACK *p1;
+    
+    
+    while (p != NULL) {
+        tmp[i++] = p->info;
+        p = p->next;
+    }
+
+    ReleaseList();
+    for (i = 0; i < g; i++) {
+        push(tmp[i]);
+    }
+}
+
+
+int CListRemoveEveryN::counter() {
+
+    STACK *p = head;
+    int i = 0;
+    
+	while (p) {
+        p = p->next;
+        ++i;
+    }
+    return i;
+}
