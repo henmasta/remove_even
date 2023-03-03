1601ca616c2a89085b60fca5c0936e8f8726660a
Henmasta <39940952+henmasta@users.noreply.github.com>
2023-03-03 10:41:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,111 +1,73 @@
+#include <iostream>
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <conio.h>
 #include "MainClass.h"
 
-#define STACK struct STACK
-
-
-STACK {
-	int info;
-	STACK *next;
-};
-
-void push(STACK **s, int item) {
-
-	STACK *new_item;
-    new_item = (STACK*) malloc(sizeof(STACK));
-	new_item -> info = item;
-    new_item -> next = *s;
-	*s = new_item;
-}
-
-int pop(STACK **s, int *error) {
-	    
-    STACK *old_item = *s;
-	int old_info = 0;
-	*error = 1;
-	if (*s) {
-        old_info = old_item -> info;
-        *s = (*s) -> next;
-        free(old_item);
-        *error = 0;
-	}
-
-    return old_info;
-}
-
-int peek(STACK **s, int *error) {
-
-    if (*s) {
-
-		*error = 0;
-        return (*s) -> info;
-
-	} else {
-
-		*error = 1;
-        return 0;
-
-	}
-}
-
-void printt(STACK* head) {
-    
-	while (head) {
-        printf("%d->", head->info);
-        head = head->next;
-    }
-
-    printf("NULL");
-}
-
-STACK *remove_even(STACK *head, int n) {
-
-    while (head && (head->info % n) == 0) {
-        STACK *p = head;
-        head = head->next;
-        delete p;
-    }
-    if (!head) return NULL;
-    
-
-    STACK *p = head;
-    while (p->next) {
-        if ((p->next->info % n) != 0) p = p->next;
-        else {
-            STACK *p2 = p->next;
-            p->next = p2->next;
-            p = p->next;
-            delete p2;
-        }
-    }
-    return head;
-}
-
-STACK *st = NULL;
+using namespace std;
 
 int main() {
-
+    
+    int key, val, n;
     CListRemoveEveryN LREN;
 
-    LREN.push(5);
-    LREN.push(4);
-    LREN.push(3);
-    LREN.push(2);
+
+
     LREN.push(1);
+    LREN.push(2);
+    LREN.push(3);
+    LREN.push(4);
 
-    //push(&st, 5);
-    //push(&st, 4);
-    //push(&st, 3);
-    //push(&st, 2);
-    //push(&st, 1);
+    do {
+        cout << "\n1 - Add element\n";
+        cout << "2 - Print stack\n";
+        cout << "3 - Clear stack\n";
+        cout << "4 - Remove Even\n";
+        cout << "ESC or 5 - Exit\n";
+        cout << "Enter: ";
+        
+        key = cin.get();
+        
+        switch(key) {
+            
+            case '1':
+            
+                cout << "Enter element: ";
+                cin >> val;
+                LREN.push(val);
+                cout << "New Stack: " << endl;
+                LREN.print();
+                break;
 
-    //remove_even(st, 3);
-    LREN.print();
-    LREN.remove_even(2);
-    //print(st);
+            case '2':
 
+                LREN.print();
+                break;
+            case '3':
+                LREN.ReleaseList();
+                break;
+            case '4':
+                do {
+                    cout << "Remove Even: ";
+                    cin >> n;
+                    if (!n && n < 2) cout << "UNCORRECT!";
+                } while(!n && n < 2);
+
+                LREN.remove_even(n);
+                break;
+            case '5':
+                cout << "Goodbye)"<< endl;
+                return 0;
+                break;
+            case '6':
+                LREN.ReverseStack();
+                break; 
+            default:
+                cout << "Invalid command";
+                break;
+        }
+        cin.get();
+        system("cls");
+    } while(key != 27);
 
 } 
